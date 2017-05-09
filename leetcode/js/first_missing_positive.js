c97ad69c79a932e846a8ba98f29085f6ea7e7586
amory-peng <amorypeng@gmail.com>
2017-05-09 16:22:00 -0700

    1 missing positive refactored

--- leetcode/js/first_missing_positive.js
+++ leetcode/js/first_missing_positive.js
@@ -1,9 +1,7 @@
 let firstMissingPositive = function(nums) {
   for (let i = 0; i < nums.length; i++) {
-    while (nums[i] > 0 && nums[i] < nums.length && nums[nums[i] - 1] !== nums[i]) {
-      let temp = nums[i];
-      nums[i] = nums[nums[i] - 1];
-      nums[nums[i] - 1] = temp;
+    while (nums[i] > 0 && nums[i] <= nums.length && nums[nums[i] - 1] !== nums[i]) {
+      swap(nums, nums[i], nums[nums[i] - 1]);
     }
   }
 
@@ -13,4 +11,10 @@ let firstMissingPositive = function(nums) {
     }
   }
   return nums.length + 1;
+};
+
+const swap = function(arr, i, j) {
+  let temp = arr[i];
+  arr[i] = arr[j];
+  arr[j] = temp;
 };
