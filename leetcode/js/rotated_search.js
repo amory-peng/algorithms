20610343874acab323fe854817b7179cbe36c6ee
amory-peng <amorypeng@gmail.com>
2017-05-10 19:04:13 -0700

    rotated arr p2

--- leetcode/js/rotated_search.js
+++ leetcode/js/rotated_search.js
@@ -1,3 +1,17 @@
 const search = function(arr, target) {
+  if (arr.length <= 0) return -1;
+  const mid = arr.length / 2;
+  //found target
+  if (target === arr[mid]) {
+    return mid;
+  } else {
+    //find which section is ordered correctly
+    if (arr[mid] > arr[0]) {
+      //if true, then left side is ordered correctly
+      if (arr[mid] > target && target > arr[0]) {
+        search(arr.slice(0, mid), target)
+      }
+    }
+  }
 
 };
